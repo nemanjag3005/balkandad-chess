554fe6bf053780498169eea577fb6fd841840bdc
Nemanja Grujic <nemanjagrujic@MacBook-Pro-2.local>
2024-07-11 16:56:00 +0200

    init navbar

---
+++ src/app/_components/Layout/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between px-10 py-5">
+      <div>Logo</div>
+      <ul></ul>
+    </nav>
+  );
+};
+
+export default Navbar;
