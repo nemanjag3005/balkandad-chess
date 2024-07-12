f47079718e24d970ceac9efdaf286077d80a31dc
Nemanja Grujic <nemanjagrujic@MacBook-Pro-2.local>
2024-07-12 18:51:20 +0200

    work on supabase

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,34 +1,36 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "~/components/ui/button";
 
+const routes: { title: string; href: string }[] = [
+  { title: "Home", href: "/" },
+  { title: "About", href: "/about" },
+  { title: "Courses", href: "/courses" },
+  { title: "Podcast", href: "/podcast" },
+  { title: "Balkan Repertoires", href: "/balkan-repertoires" },
+  { title: "Blog", href: "/blog" },
+];
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <nav className="sticky top-0 flex items-center justify-center px-10 py-5">
       <div className="flex w-full max-w-7xl items-center justify-between">
         <div>Logo</div>
         <ul className="hidden items-center justify-between space-x-6 md:flex">
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/about">
-            <li>About</li>
-          </Link>
-          <Link href="/about">
-            <li>Courses</li>
-          </Link>
-          <Link href="/about">
-            <li>Podcast</li>
-          </Link>
-          <Link href="/about">
-            <li>Balkan Repertoires</li>
-          </Link>
-          <Link href="/about">
-            <li>Blog</li>
-          </Link>
-          <Link href="/about">
-            <li>Newsletter</li>
-          </Link>
+          {routes.map((route) => (
+            <Link key={route.href} href={route.href}>
+              <li>{route.title}</li>
+            </Link>
+          ))}
+
+          <li>Newsletter</li>
+
           <Button>Login</Button>
         </ul>
       </div>
