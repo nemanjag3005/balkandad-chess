bf60ba00668e19e7d6a63a8ff1ee682bdae26f66
nemanjag3005 <nemanjag3005@gmail.com>
2024-08-05 12:01:54 +0200

    fix v2

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -3,27 +3,28 @@
 import { createClient } from "~/utils/supabase/server";
 import { headers } from "next/headers";
 import { LoginInput } from "./login/page";
+import { SignupInput } from "./checkout/page";
 
 const supabase = createClient();
 const origin = headers().get("origin");
 
-// export const signUp = async (data: SignupInput) => {
-//   "use server";
+export const signUp = async (data: SignupInput) => {
+  "use server";
 
-//   const { error } = await supabase.auth.signUp({
-//     email: data.email,
-//     password: data.password,
-//     options: {
-//       emailRedirectTo: `${origin}/auth/callback`,
-//     },
-//   });
+  const { error } = await supabase.auth.signUp({
+    email: data.email,
+    password: data.password,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
+  });
 
-//   if (error) {
-//     return {
-//       error: error.message,
-//     };
-//   }
-// };
+  if (error) {
+    return {
+      error: error.message,
+    };
+  }
+};
 
 export const signIn = async (data: LoginInput) => {
   "use server";
